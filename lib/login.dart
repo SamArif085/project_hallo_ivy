6aea595b34589b9ed8a9ddba6f56ef3471628270
SamArif <samarif085@gmail.com>
2023-06-21 23:58:47 +0700

    Initial commit

---
+++ lib/login.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    const logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Center(
+            child: Text(
+              'HALLO IVY',
+              style: TextStyle(fontSize: 50, color: Colors.black),
+            ),
+          )),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        hintText: 'Username',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: () => Navigator.pushNamed(context, "/"),
+          child: const Text('Log In', style: TextStyle(color: Colors.black)),
+        ),
+      ),
+    );
+
+    final forgotLabel = TextButton(
+      child: const Text(
+        'Forgot password?',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {},
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Card(
+              color: Colors.greenAccent[400],
+              elevation: 5,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: ListView(
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                children: <Widget>[
+                  const SizedBox(height: 48.0),
+                  logo,
+                  const SizedBox(height: 48.0),
+                  email,
+                  const SizedBox(height: 8.0),
+                  password,
+                  const SizedBox(height: 24.0),
+                  loginButton,
+                  forgotLabel
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  'assets/ak.png',
+                  height: 80,
+                ),
+                Image.asset(
+                  'assets/patreon.png',
+                  height: 80,
+                ),
+                Image.asset(
+                  'assets/jatim.png',
+                  height: 80,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
